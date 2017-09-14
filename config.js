a45fce512870cc0445d3da002ca830e47cd92eba
Adam Jones <adam@enmaso.com>
2017-09-13 18:36:31 -0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "web": {
+    "port": 9000
+  },
+  "ner": {
+    "port": 8080
+  }
+}
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const net = require('net');
 const path = require('path');
 const childProcess = require('child_process');
 
+const config = require('./config');
+
 childProcess.exec('java -version', (err, stdout, stderr) => {
   if(err) {
     console.error('Java Runtime Required');
@@ -10,17 +12,17 @@ childProcess.exec('java -version', (err, stdout, stderr) => {
   }
 });
 
-const HTTP_PORT = process.argv[2] || 9000;
-const NER_PORT = process.argv[3] || 8080;
+// const HTTP_PORT = process.argv[2] || 9000;
+// const NER_PORT = process.argv[3] || 8080;
 
-const child = childProcess.spawn('./bin/server.sh', [`${NER_PORT}`]);
+const child = childProcess.spawn('./bin/server.sh', [`${config.ner.port}`]);
 
 const server = http.createServer((req, res) => {
   let body = '';
   req.on('data', data => {
     body += data;
     // Too much POST data, kill the connection!
-    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+    // 2e6 === 2 * Math.pow(10, 6) === 2 * 1000000 ~~~ 2MB
     if (body.length > 2e6) {
       req.connection.destroy();
     }
@@ -28,7 +30,7 @@ const server = http.createServer((req, res) => {
   req.on('end', () => {
     let socket = new net.Socket();
     res.setHeader('Content-Type', 'application/json');
-    socket.connect(NER_PORT, 'localhost', () => {
+    socket.connect(config.ner.port, 'localhost', () => {
       socket.write(body.replace(/\r?\n|\r|\t/g, ' ') + '\n');
     });
     socket.on('data', data => {
@@ -36,20 +38,20 @@ const server = http.createServer((req, res) => {
       let str = data.toString();
       let tags = {
         LOCATION: [],
-  			ORGANIZATION: [],
-  			DATE: [],
-  			MONEY: [],
-  			PERSON: [],
-  			PERCENT: [],
-  			TIME: []
+        ORGANIZATION: [],
+        DATE: [],
+        MONEY: [],
+        PERSON: [],
+        PERCENT: [],
+        TIME: []
       };
       let m;
-  		while ((m = regexp.exec(str)) !== null) {
-  			if (m.index === regexp.lastIndex) {
-  				regexp.lastIndex++;
-  			}
-  			tags[m[1]].push(m[2]);
-  		}
+      while ((m = regexp.exec(str)) !== null) {
+        if (m.index === regexp.lastIndex) {
+          regexp.lastIndex++;
+        }
+        tags[m[1]].push(m[2]);
+      }
       socket.destroy();
       res.end(JSON.stringify(tags));
     });
@@ -61,8 +63,8 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(HTTP_PORT, (err) => {
-  console.log(`server listening on ${HTTP_PORT}`);
+server.listen(config.web.port, (err) => {
+  console.log(`server listening on ${config.web.port}`);
 });
 
 process.on('exit', () => {
