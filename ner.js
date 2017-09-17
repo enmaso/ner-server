9039c8d2c096bf0b568ca4423d46eebf430077fc
Enmaso Inc <adam@enmaso.com>
2017-09-16 20:50:08 -0700

    Add test

--- ner.js
+++ ner.js
@@ -1,45 +1,33 @@
-const http = require('http');
-const net = require('net');
-const path = require('path');
-const childProcess = require('child_process');
+const http = require('http')
+const net = require('net')
+const spawn = require('child_process').spawn
 
-childProcess.exec('java -version', (err, stdout, stderr) => {
-  if(err) {
-    console.error('Java Runtime Required');
-    process.exit(1);
-  }
-});
+const hport = process.env.HTTP_PORT || 8080
+const tport = process.env.TCP_PORT || 8081
 
-const hport = process.env.HPORT || 9000;
-const nport = process.env.NPORT || 8080;
-const limit = process.env.LIMIT || 2;
-
-const child = childProcess.spawn('./bin/server.sh', [`${nport}`]);
+const child = spawn('./bin/server.sh', [`${tport}`])
 const server = http.createServer((req, res) => {
-  let body = '';
-  if(req.method !== 'PUT') {
-    res.statusCode = 400;
-    return res.end('HTTP PUT');
+  let file = ''
+  if (req.method !== 'PUT') {
+    res.statusCode = 400
+    res.end('NER Server (Stanford NER). Please PUT')
   }
   req.on('data', data => {
-    body += data;
-    if (body.length > (limit * Math.pow(10, 6))) {
-      req.connection.destroy();
-    }
-  });
+    file += data
+  })
   req.on('end', () => {
-    if(body.length == 0) {
-      res.statusCode = 400;
-      return res.end('HTTP PUT');
+    if (file.length === 0) {
+      res.statusCode = 400
+      return res.end('NER Server (Stanford NER). Please PUT')
     }
-    let socket = new net.Socket();
-    res.setHeader('Content-Type', 'application/json');
-    socket.connect(nport, 'localhost', () => {
-      socket.write(body.replace(/\r?\n|\r|\t/g, ' ') + '\n');
-    });
+    let socket = new net.Socket()
+    res.setHeader('Content-Type', 'application/json')
+    socket.connect(tport, 'localhost', () => {
+      socket.write(file.replace(/\r?\n|\r|\t/g, ' ') + '\n')
+    })
     socket.on('data', data => {
-      let regexp = /<([A-Z]+?)>(.+?)<\/\1>/g;
-      let str = data.toString();
+      let regexp = /<([A-Z]+?)>(.+?)<\/\1>/g
+      let str = data.toString()
       let tags = {
         LOCATION: [],
         ORGANIZATION: [],
@@ -48,39 +36,48 @@ const server = http.createServer((req, res) => {
         PERSON: [],
         PERCENT: [],
         TIME: []
-      };
-      let m;
+      }
+      let m
       while ((m = regexp.exec(str)) !== null) {
         if (m.index === regexp.lastIndex) {
-          regexp.lastIndex++;
+          regexp.lastIndex++
         }
-        tags[m[1]].push(m[2]);
+        tags[m[1]].push(m[2])
       }
-      socket.destroy();
-      res.end(JSON.stringify(tags));
-    });
+      socket.destroy()
+      res.end(JSON.stringify(tags))
+    })
     socket.on('error', err => {
       res.end(JSON.stringify({
         error: err
-      }));
-    });
-  });
-});
-
-server.listen(hport, (err) => {
-  console.log(`ner-server listening on ${hport}`);
-});
+      }))
+    })
+  })
+})
 
 process.on('exit', () => {
-  console.log('');
-  console.log('Exiting Web server');
-  console.log('Exiting STANFORD NER server');
-  child.kill('SIGINT');
-});
+  console.log('')
+  console.log('Exiting Web server')
+  console.log('Exiting STANFORD NER server')
+  child.kill('SIGINT')
+})
 process.on('SIGINT', () => {
-  process.exit();
-});
+  process.exit()
+})
 process.on('uncaughtException', e => {
-  console.log(e.stack);
-  process.exit(99);
-});
+  console.log(e.stack)
+  process.exit(99)
+})
+
+server.listen(hport, (err) => {
+  if (err) console.log(err)
+  console.log(`ner-server listening on ${hport}`)
+})
+
+module.exports.listen = () => {
+  server.listen.apply(server, arguments)
+}
+
+module.exports.close = (callback) => {
+  server.close(callback)
+}
